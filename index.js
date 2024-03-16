21a4051576cbbfc3e97741d2df6704878188de61
Sravankumar-dev <sravankumar8522@gmail.com>
2024-03-16 13:10:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let headEl = document.getElementById("heading");
+let btnEl = document.getElementById("color");
+
+btnEl.addEventListener("click",function(){
+    headEl.style.color="blue";
+    headEl.style.textAlign = "center";
+    btnEl.style.textAlign = "center";
+
+})
